365b0abe0e955ab8e6a479379719400cf31db1e1
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2026-09-07 16:45:33 +0300

    Fix possible stack overflow error (#3717)
    * Fix possible stack overflow error
    
    Should fix:
    ```
    - addons/server-scripts/lua/autorun/!!!_sh_performance.lua:137: stack overflow
    1. __index - addons/server-scripts/lua/autorun/!!!_sh_performance.lua:137
     2. BackupRenderOverride - addons/wire/lua/wire/flir.lua:52
      3. BackupRenderOverride - addons/wire/lua/wire/flir.lua:53
       4. BackupRenderOverride - addons/wire/lua/wire/flir.lua:53
        5. BackupRenderOverride - addons/wire/lua/wire/flir.lua:53
         6. BackupRenderOverride - addons/wire/lua/wire/flir.lua:53
          7. BackupRenderOverride - addons/wire/lua/wire/flir.lua:53
           8. BackupRenderOverride - addons/wire/lua/wire/flir.lua:53
            9. BackupRenderOverride - addons/wire/lua/wire/flir.lua:53
             10. BackupRenderOverride - addons/wire/lua/wire/flir.lua:53
              11. BackupRenderOverride - addons/wire/lua/wire/flir.lua:53
               12. BackupRenderOverride - addons/wire/lua/wire/flir.lua:53
                13. BackupRenderOverride - addons/wire/lua/wire/flir.lua:53
                 14. BackupRenderOverride - addons/wire/lua/wire/flir.lua:53
                  15. BackupRenderOverride - addons/wire/lua...
    ```
    
    * Fix potential memory leak
    
    * Don't remove if dormant

--- lua/wire/flir.lua
+++ lua/wire/flir.lua
@@ -98,7 +98,7 @@ local function RemoveFLIR(ent)
 end
 
 local function SetFLIR(ent)
-	if not ent:IsValid() then return end
+	if not ent:IsValid() or FLIR.RenderStack[ent] then return end
 	local classname = ent:GetClass()
 
 	if ent:GetColor().a > 0 and (FLIR.classWhitelist[classname] or ent:GetMoveType() == MOVETYPE_VPHYSICS or ent:IsPlayer() or ent:IsNPC() or ent:IsRagdoll()) then
@@ -133,11 +133,13 @@ function FLIR.start()
 		render.SuppressEngineLighting(true)
 		render.SetColorModulation(FLIR.gain, FLIR.gain, FLIR.gain)  			--this works?? I could not for the life of me make it work in renderoverride. Well.
 																				--It's a much better solution than the stencil I spent hours on...
-		for ent, valid in pairs(FLIR.RenderStack) do
-			if valid and ent:IsValid() and not ent:GetNoDraw() then
-				FLIR.hide = false
-				ent:DrawModel()
-				FLIR.hide = true
+		for ent in pairs(FLIR.RenderStack) do
+			if ent:IsValid() and not ent:GetNoDraw() then
+				if not ent:IsDormant() then
+					FLIR.hide = false
+					ent:DrawModel()
+					FLIR.hide = true
+				end
 			else
 				RemoveFLIR(ent)
 			end
@@ -145,7 +147,6 @@ function FLIR.start()
 
 		render.SuppressEngineLighting(false)
 		render.MaterialOverride(FLIR.transmat)
-
 	end)
 
 	hook.Add("PostDrawTranslucentRenderables", "wire_flir", function()
@@ -190,8 +191,8 @@ function FLIR.stop()
 	hook.Remove("OnEntityCreated", "wire_flir")
 	hook.Remove("CreateClientsideRagdoll", "wire_flir")
 
-	for _, v in ents.Iterator() do
-		RemoveFLIR(v)
+	for ent in pairs(FLIR.RenderStack) do
+		RemoveFLIR(ent)
 	end
 end
 
@@ -211,4 +212,3 @@ net.Receive("FLIR.enable", function()
 	local enabled = net.ReadBool()
 	FLIR.enable(enabled)
 end)
-
